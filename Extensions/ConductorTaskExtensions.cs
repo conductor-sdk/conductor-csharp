79d311c0058165a38f7e14bb14fd0184ff502801
Manan Bhatt <manan16489@gmail.com>
2022-04-10 11:04:26 +0530

    Working code.

--- Extensions/ConductorTaskExtensions.cs
+++ Extensions/ConductorTaskExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Conductor.Client.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Conductor.Client.Extensions
 {
@@ -10,67 +9,67 @@ namespace Conductor.Client.Extensions
         public static TaskResult InProgress(this Task task, string log = null, long? callbackAfterSeconds = null, Dictionary<string, object> outputData = null)
         {
             return new TaskResult
-            {
-                WorkflowInstanceId = task.WorkflowInstanceId,
-                TaskId = task.TaskId,
-                Status = TaskResult.StatusEnum.INPROGRESS,
-                OutputData = outputData,
-                Logs =  new List<TaskExecLog>
+            (
+                workflowInstanceId : task.WorkflowInstanceId,
+                taskId : task.TaskId,
+                status : TaskResult.StatusEnum.INPROGRESS,
+                outputData : outputData,
+                logs :  new List<TaskExecLog>
                 {
                     new TaskExecLog { TaskId = task.TaskId, Log = log, CreatedTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() }
                 },
-                CallbackAfterSeconds = callbackAfterSeconds.Value
-            };
+                callbackAfterSeconds : callbackAfterSeconds.Value
+            );
         }
 
         public static TaskResult InProgress(this Task task, Dictionary<string, object> outputData = null, List<TaskExecLog> logs = null)
         {
             return new TaskResult
-            {
-                WorkflowInstanceId = task.WorkflowInstanceId,
-                TaskId = task.TaskId,
-                Status = TaskResult.StatusEnum.INPROGRESS,
-                OutputData = outputData,
-                Logs = logs
-            };
+            (
+                workflowInstanceId : task.WorkflowInstanceId,
+                taskId : task.TaskId,
+                status : TaskResult.StatusEnum.INPROGRESS,
+                outputData : outputData,
+                logs : logs
+            );
         }
 
         public static TaskResult Completed(this Task task, Dictionary<string, object> outputData = null, List<TaskExecLog> logs = null)
         {
             return new TaskResult
-            {
-                WorkflowInstanceId = task.WorkflowInstanceId,
-                TaskId = task.TaskId,
-                Status = TaskResult.StatusEnum.COMPLETED,
-                OutputData = outputData,
-                Logs = logs
-            };
+            (
+                workflowInstanceId : task.WorkflowInstanceId,
+                taskId : task.TaskId,
+                status : TaskResult.StatusEnum.COMPLETED,
+                outputData : outputData,
+                logs : logs
+            );
         }
 
         public static TaskResult Failed(this Task task, string errorMessage, Dictionary<string, object> outputData = null, List<TaskExecLog> logs = null)
         {
             return new TaskResult
-            {
-                WorkflowInstanceId = task.WorkflowInstanceId,
-                TaskId = task.TaskId,
-                Status = TaskResult.StatusEnum.FAILED,
-                ReasonForIncompletion = errorMessage,
-                OutputData = outputData,
-                Logs = logs
-            };
+            (
+                workflowInstanceId : task.WorkflowInstanceId,
+                taskId : task.TaskId,
+                status : TaskResult.StatusEnum.FAILED,
+                reasonForIncompletion : errorMessage,
+                outputData : outputData,
+                logs : logs
+            );
         }
 
         public static TaskResult FailedWithTerminalError(this Task task, string errorMessage, Dictionary<string, object> outputData = null, List<TaskExecLog> logs = null)
         {
             return new TaskResult
-            {
-                WorkflowInstanceId = task.WorkflowInstanceId,
-                TaskId = task.TaskId,
-                Status = TaskResult.StatusEnum.FAILEDWITHTERMINALERROR,
-                ReasonForIncompletion = errorMessage,
-                OutputData = outputData,
-                Logs = logs
-            };
+            (
+                workflowInstanceId : task.WorkflowInstanceId,
+                taskId : task.TaskId,
+                status : TaskResult.StatusEnum.FAILEDWITHTERMINALERROR,
+                reasonForIncompletion : errorMessage,
+                outputData : outputData,
+                logs : logs
+            );
         }
     }
 }
