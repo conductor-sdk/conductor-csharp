9f3168d9f7ca70e554dd67e3dd351a19bc86083a
gardusig <gustavo.gardusi@gmail.com>
2022-10-28 00:08:44 -0300

    Added script to generate code for base client

--- script/sync_conductor_apis.sh
+++ script/sync_conductor_apis.sh
@@ -25,74 +25,88 @@ function undo_line_ending_replacement {
     tr $REPLACEMENT_FOR_LINE_ENDING '\n' <"$1" >"$TEMPORARY_FILE" && mv "$TEMPORARY_FILE" "$1"
 }
 
-function copy_from_generated_files {
-    local source_path="${SWAGGER_GENERATED_CODE_FOLDER}/${SWAGGER_GENERATED_CODE_PACKAGE}/${1}"
-    local destination_path="${PACKAGE_PATH}/${1}"
+function remove_empty_lines_prefix_from_file {
+    local path="${1}"
+    sed -i "" '/./,$!d' "${file}"
+}
+
+function replace_package_files {
+    local path="${1}"
+    local source_path="${SWAGGER_GENERATED_CODE_FOLDER}/${SWAGGER_GENERATED_CODE_PACKAGE}/${path}"
+    local destination_path="${PACKAGE_PATH}/${path}"
+    echo "starting to replace generated files from ${source_path} to ${destination_path}"
     rm -rf "${destination_path:?}/"*
     echo "deleted all files from ${destination_path}"
     for filename in "${source_path}/"*; do
-        cp "${source_path}/${filename}" "${destination_path}/"
+        cp "${filename}" "${destination_path}/"
     done
-    echo "copied code from ${source_path} to ${destination_path}"
+    echo "done copying code from ${source_path} to ${destination_path}"
 }
 
-# function replace_with_sed {
-#     sed -i '' -E s/"$1"/"$2"/g "$3"
-# }
+function replace_with_sed {
+    local filepath="${1}"
+    local pattern_regex="$2"
+    local replacement="${3}"
+    sed -i '' -E s/"${pattern_regex}"/"${replacement}"/g "${filepath}"
+}
 
-# function remove_file_header {
-#     pattern="${2}"
-#     replace="${3}"
-#     replace_with_sed "$pattern" "$replace" "$1"
-# }
+function remove_header_from_file {
+    local filepath="${1}"
+    replace_with_sed "${filepath}" "^\/\*.*\*\/" ""
+    sed -i '' '/./,$!d' "${filepath}"
+}
 
-# function replace_import_header {
-#     pattern="from swagger_client.api_client import ApiClient"
-#     replace="from conductor.client.http.api_client import ApiClient"
-#     replace_with_sed "$pattern" "$replace" "$1"
-# }
+function replace_namespace_from_file {
+    local filepath="${1}"
+    replace_with_sed "${filepath}" "IO.Swagger.Client" "Conductor"
+}
 
-# function replace_authentication {
-#     pattern="'api_key'"
-#     replace=""
-#     replace_with_sed "$pattern" "$replace" "$1"
-# }
+function update_package_file {
+    local path="${1}"
+    local update_package_file_function=${2}
+    echo "starting to update package file: ${file}"
+    do_line_ending_replacement "${file}"
+    ${update_package_file_function} "${file}"
+    undo_line_ending_replacement "${file}"
+    remove_empty_lines_prefix_from_file "${file}"
+    echo "done updating package: ${file}"
+}
 
-# function replace_url_api_prefix {
-#     pattern="\'\/api"
-#     replace="\'"
-#     replace_with_sed "$pattern" "$replace" "$1"
-# }
+function update_package_files {
+    local package_to_update="${1}"
+    local update_package_file_function=${2}
+    local path="${PACKAGE_PATH}/${package_to_update}"
+    echo "starting to update package files: ${path}"
+    for file in "${path}/"*; do
+        update_package_file "${file}" ${update_package_file_function}
+    done
+    echo "done updating package files: ${path}"
+}
+
+function update_package {
+    local package_to_update="${1}"
+    local update_package_file_function=${2}
+    local startup_function=${3}
+    echo "starting to update package: ${package_to_update}"
+    replace_package_files "${package_to_update}"
+    if [ -n "${startup_function}" ]; then
+        ${startup_function}
+    fi
+    update_package_files "${package_to_update}" ${update_package_file_function}
+    echo "done updating package: ${package_to_update}"
+}
 
 function update_client_package_startup {
-    echo "starting to update client package"
+    echo "startup for client package"
 }
 
 function update_client_package_file {
     local filepath="${1}"
-    echo "updating client file: ${filepath}"
-}
-
-function update_package_files {
-    echo "starting to update package ${package_to_update} files..."
-    local package_to_update="${1}"
-    local update_package_file_function=${2}
-    local startup_function=${3}
-    copy_from_generated_files "${package_to_update}"
-    if [ ! -z "${startup_function}" ]; then
-        ${startup_function}
-    fi
-    for filename in "${PACKAGE_PATH}/${package_to_update}/"*; do
-        local filepath="${PACKAGE_PATH}/${package_to_update}/${filename}"
-        do_line_ending_replacement "${filepath}"
-        ${update_package_file_function} "${filepath}"
-        undo_line_ending_replacement "${filepath}"
-        echo "done updating: ${filepath}"
-    done
-    echo "done updating package ${package_to_update} files"
+    remove_header_from_file "${filepath}"
+    replace_namespace_from_file "${filepath}"
 }
 
 # install_dependencies
 # generate_code
 
-update_package_files "Client" update_client_package_file update_client_package_startup
+update_package "Client" update_client_package_file update_client_package_startup
