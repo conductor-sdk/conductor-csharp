48604937b1ee90596dadedc1c0973c16ca7ff4b6
gardusig <gustavo.gardusi@gmail.com>
2022-11-06 18:59:43 -0300

    Added boilerplate for e2e test

---
+++ Tests/Worker/WorkerTests.cs
@@ -0,0 +1,20 @@
+using Tests.Util;
+using Xunit;
+
+
+namespace Tests.Worker
+{
+    public class WorkerTests : IntegrationTest
+    {
+        public WorkerTests()
+        {
+
+        }
+
+        [Fact]
+        public void TestWorkflowAsyncExecution()
+        {
+            // TODO
+        }
+    }
+}
