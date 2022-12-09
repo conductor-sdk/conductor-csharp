b6c7c665e3c6ad800c9ae4776bcd7fde053f2322
gardusig <gustavo.gardusi@gmail.com>
2022-12-09 15:53:51 -0300

    Created simple worker example for test usage

---
+++ Tests/Util/SimpleWorker.cs
@@ -0,0 +1,34 @@
+using Conductor.Client.Models;
+using Conductor.Client.Interfaces;
+using Conductor.Client.Extensions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tests.Util
+{
+    public class SimpleWorker : IWorkflowTask
+    {
+        public string TaskType { get; }
+        public int? Priority { get; }
+
+        public SimpleWorker(string taskType)
+        {
+            TaskType = taskType;
+        }
+
+        public Task<TaskResult> Execute(Conductor.Client.Models.Task task, CancellationToken token)
+        {
+            return new System.Threading.Tasks.Task<TaskResult>(
+                () =>
+                {
+                    return Execute(task);
+                }
+            );
+        }
+
+        private static TaskResult Execute(Conductor.Client.Models.Task task)
+        {
+            return ConductorTaskExtensions.Completed(task);
+        }
+    }
+}
