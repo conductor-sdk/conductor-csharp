04c43120794bd8f18a38e20d3ec25cf42f9261af
gardusig <gustavo.gardusi@gmail.com>
2022-12-09 18:14:35 -0300

    Added worker tests

--- Conductor/Client/Extensions/DependencyInjectionExtensions.cs
+++ Conductor/Client/Extensions/DependencyInjectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Conductor.Client.Interfaces;
+﻿using Microsoft.Extensions.Hosting;
+using Conductor.Client.Interfaces;
 using Conductor.Client.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -19,11 +20,25 @@ namespace Conductor.Client.Extensions
         {
             services.AddHttpClient();
             services.AddOptions();
+            services.AddSingleton(new OrkesApiClient(configuration));
             services.AddTransient<IConductorWorkerRestClient, ConductorWorkerRestClient>();
             services.AddSingleton(configuration != null ? configuration : new Configuration());
             services.AddSingleton<IWorkflowTaskCoordinator, WorkflowTaskCoordinator>();
             services.AddTransient<IWorkflowTaskExecutor, WorkflowTaskExecutor>();
             return services.AddConductorClient(configureHttpClient);
+        }
+
+        public static IServiceCollection AddWorkflowsWorkerService(this IServiceCollection services, BackgroundService backgroundService)
+        {
+            services.AddSingleton(backgroundService);
+            return services;
+        }
+
+        public static IServiceCollection AddWorkflowTaskExecutor<T>(this IServiceCollection services) where T : IWorkflowTask
+        {
+            services.AddTransient(typeof(IWorkflowTask), typeof(T));
+            services.AddTransient(typeof(T));
+            return services;
         }
 
         public static IServiceCollection AddConductorClient(this IServiceCollection services, Func<IServiceProvider, string> serverUrl)
--- Tests/Worker/WorkerTests.cs
+++ Tests/Worker/WorkerTests.cs
@@ -1,13 +1,103 @@
+using Conductor.Client.Extensions;
+using Conductor.Client.Interfaces;
+using Conductor.Definition;
+using Conductor.Api;
+using Conductor.Definition.TaskType;
+using Conductor.Executor;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Tests.Util;
+using System.Threading;
+using System.Collections.Generic;
 using Xunit;
+
+using System;
 
 namespace Tests.Worker
 {
     public class WorkerTests
     {
+        private const string WORKFLOW_NAME = "test-sdk-csharp-worker";
+        private const int WORKFLOW_VERSION = 1;
+
+        private const string TASK_NAME = "test-sdk-csharp-task";
+        private const int WORKFLOW_EXECUTION_TIMEOUT_SECONDS = 5;
+
+        private WorkflowExecutor _workflowExecutor = null;
+
+        public WorkerTests()
+        {
+            _workflowExecutor = ApiUtil.GetWorkflowExecutor();
+        }
+
         [Fact]
         public void TestWorkflowAsyncExecution()
         {
-            // TODO
+            ConductorWorkflow workflow = GetConductorWorkflow();
+            _workflowExecutor.RegisterWorkflow(workflow, true);
+            StartWorkers();
+            string workflowId = _workflowExecutor.StartWorkflow(workflow);
+            Console.WriteLine("workflowId: " + workflowId);
+            Thread.Sleep(WORKFLOW_EXECUTION_TIMEOUT_SECONDS * 1000);
+            WorkflowResourceApi workflowClient = ApiUtil.GetClient<WorkflowResourceApi>();
+            Conductor.Client.Models.WorkflowStatus workflowStatus = workflowClient.GetWorkflowStatusSummary(workflowId);
+            Assert.Equal(
+                Conductor.Client.Models.WorkflowStatus.StatusEnum.COMPLETED,
+                workflowStatus.Status
+            );
+        }
+
+        private ConductorWorkflow GetConductorWorkflow()
+        {
+            return new ConductorWorkflow()
+                .WithName(WORKFLOW_NAME)
+                .WithVersion(WORKFLOW_VERSION)
+                .WithTask(
+                    new SimpleTask(TASK_NAME, TASK_NAME)
+                );
+        }
+
+        private void StartWorkers()
+        {
+            //     System.Threading.Tasks.Task host = new HostBuilder()
+            //         .ConfigureServices(
+            //             (ctx, services) =>
+            //                 {
+            //                     services.AddConductorWorker(ApiUtil.GetApiClient());
+            //                     services.AddConductorWorkflowTask<SimpleWorker>();
+            //                     services.AddWorkflowsWorkerService<WorkflowsWorkerService>();
+            //                 }
+            //         ).ConfigureLogging(
+            //             logging =>
+            //                 {
+            //                     logging.SetMinimumLevel(LogLevel.Debug);
+            //                     logging.AddConsole();
+            //                 }
+            //         ).RunConsoleAsync();
         }
     }
+
+    // internal class WorkflowsWorkerService : BackgroundService
+    // {
+    //     private readonly IWorkflowTaskCoordinator _workflowTaskCoordinator;
+    //     private readonly IEnumerable<IWorkflowTask> _workers;
+
+    //     public WorkflowsWorkerService(
+    //         IWorkflowTaskCoordinator workflowTaskCoordinator,
+    //         List<IWorkflowTask> workers
+    //     )
+    //     {
+    //         _workflowTaskCoordinator = workflowTaskCoordinator;
+    //         _workers = workers;
+    //     }
+
+    //     protected override System.Threading.Tasks.Task ExecuteAsync(CancellationToken stoppingToken)
+    //     {
+    //         foreach (var worker in _workers)
+    //         {
+    //             _workflowTaskCoordinator.RegisterWorker(worker);
+    //         }
+    //         return _workflowTaskCoordinator.Start();
+    //     }
+    // }
 }
