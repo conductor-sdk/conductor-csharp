685b4fd9b7108645c460648b9aaea158f74a44aa
Mkatari3700 <58773431+Mkatari3700@users.noreply.github.com>
2024-01-04 11:24:41 +0530

    Log changes (#99)
    Co-authored-by: Katari.Manikanta <Katari.manikanta@dynamic-wilreless.com>

---
+++ Conductor/Client/Extensions/ApplicationLogging.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace conductor.csharp.Client.Extensions
+{
+    public static class ApplicationLogging
+    {
+        public static ILoggerFactory LoggerFactory = new LoggerFactory();
+        public static ILogger CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
+        public static ILogger CreateLogger(string categoryName) => LoggerFactory.CreateLogger(categoryName);
+    }
+}
