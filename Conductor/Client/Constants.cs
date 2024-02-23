a84be6cb52551e29dabe5f014105fcc6d51f4c9e
Jithesh Poojary <Jithesh.Poojary@triconinfotech.com>
2024-02-23 16:14:05 +0530

    Updated Constants file

--- Conductor/Client/Constants.cs
+++ Conductor/Client/Constants.cs
@@ -5,9 +5,9 @@
     /// </summary>
     public static class Constants
     {
-        public const string KEY_ID = "2aa73e29-de29-479c-8b6d-5b45aa48aa4a";
-        public const string KEY_SECRET = "wygpIUK34iroLawafQzH3jgrbbMGUQvdeh46xicPvz9f6Kiq";
-        public const string OWNER_EMAIL = "jitheshpoojary04@gmail.com";
+        public const string KEY_ID = "<REPLACE_WITH_KEY_ID>";
+        public const string KEY_SECRET = "<REPLACE_WITH_KEY_SECRET>";
+        public const string OWNER_EMAIL = "<REPLACE_WITH_OWNER_EMAIL>";
         public const int REST_CLIENT_REQUEST_TIME_OUT = 20000;
         public const int MAX_TOKEN_REFRESH_RETRY_COUNT = 3;
 
